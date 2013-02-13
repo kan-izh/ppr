97d03bc402c73df7fb3e75dc45aff1485f1a3a3c
Anatoly Kupriyanov <kan.izh@gmail.com>
2013-02-13 21:46:17 +0000

    Learning guice

--- src/test/java/name/kan/ppr/test/DbModule.java
+++ src/test/java/name/kan/ppr/test/DbModule.java
@@ -1,9 +1,13 @@
 package name.kan.ppr.test;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 import org.h2.jdbcx.JdbcDataSource;
 
+import javax.inject.Named;
 import javax.sql.DataSource;
+
+import static com.google.inject.name.Names.named;
 
 /**
  * @author kan
@@ -14,9 +18,16 @@ public class DbModule extends AbstractModule
 	@Override
 	protected void configure()
 	{
-		final JdbcDataSource h2Ds = new JdbcDataSource();
-		h2Ds.setURL("jdbc:h2:mem:unit_test;DB_CLOSE_DELAY=-1");
-		bind(DataSource.class)
-				.toInstance(h2Ds);
+		bindConstant()
+				.annotatedWith(named("jdbcUrl"))
+				.to("jdbc:h2:mem:unit_test;DB_CLOSE_DELAY=-1");
+	}
+
+	@Provides
+	public DataSource getDataSource(final @Named("jdbcUrl") String jdbcUrl)
+	{
+		final JdbcDataSource ds = new JdbcDataSource();
+		ds.setURL(jdbcUrl);
+		return ds;
 	}
 }
